55e29150562e12912bc902dd40c11726ef7e2a75
KonradM <miekus.konrad@gmail.com>
2017-08-23 22:29:35 +0200

    Add Redirect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Route from './router/Route'
 import Router from './router/Router'
+import Redirect from './router/Redirect'
 
 import Test1 from './Test1'
 import Test2 from './Test2'
@@ -12,6 +13,7 @@ import './App.scss'
 const App = () => (
   <Router>
     <div>
+      <Redirect to='/test1' />
       <Home />
       <Route path='/test1' component={Test1} />
       <Route path='/test2' component={Test2} />
